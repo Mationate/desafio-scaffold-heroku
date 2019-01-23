85972604bc38a47944f380317599f864b4148383
Matias Oñate <mationate.0@gmail.com>
2019-01-23 17:08:15 -0300

    continuacion ejercicio estudiantes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,10 @@ class PagesController < ApplicationController
   def landing
     @students = Student.all
   end
+  def old
+    @old_students = Student.where("age > ?", 17)
+  end
+  def young
+    @young_students = Student.where("age < ?", 18)
+  end
 end
